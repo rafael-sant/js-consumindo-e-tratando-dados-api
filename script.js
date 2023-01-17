63ff85d55fe55cebfcf7266688980995b8a55967
Rafael Santos <rafaelsaannttos@gmail.com>
2023-01-17 15:17:11 -0300

    making requisitions with async await

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-var consultaCep = fetch("https://viacep.com.br/ws/01001000/json/")
-  .then((resposta) => resposta.json())
-  .then((r) => {
-    if (r.erro) {
-      throw Error("Esse cep não existe");
-    } else {
-      console.log(r);
+async function buscaEndereco(cep) {
+  try {
+    var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    var consultaCepConvertida = await consultaCep.json();
+    if (consultaCepConvertida.erro) {
+      throw Error("Cep não existente!");
     }
-  })
-  .catch((erro) => {
+    console.log(consultaCepConvertida);
+    return consultaCepConvertida;
+  } catch (erro) {
     console.log(erro);
-  })
-  .finally((mensagem) => console.log("Processamento concluído"));
-
-console.log(consultaCep);
+  }
+}
