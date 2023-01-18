7396dd002f048d23e41b635a4b806e46e88d67d3
Rafael Santos <rafaelsaannttos@gmail.com>
2023-01-18 00:20:05 -0300

    done training - manipulating data on form

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 async function buscaEndereco(cep) {
+  var mensagemErro = document.getElementById("erro");
+  mensagemErro.innerHTML = "";
+
   try {
     var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     var consultaCepConvertida = await consultaCep.json();
     if (consultaCepConvertida.erro) {
       throw Error("Cep não existente!");
     }
+    var cidade = document.getElementById("cidade");
+    var logradouro = document.getElementById("endereco");
+    var estado = document.getElementById("estado");
+
+    cidade.value = consultaCepConvertida.localidade;
+    logradouro.value = consultaCepConvertida.logradouro;
+    estado.value = consultaCepConvertida.uf;
+
     console.log(consultaCepConvertida);
     return consultaCepConvertida;
   } catch (erro) {
+    mensagemErro.innerHTML = "<p> CEP inválido </p>";
     console.log(erro);
   }
 }
+
+var cep = document.getElementById("cep");
+
+cep.addEventListener("focusout", () => {
+  buscaEndereco(cep.value);
+});
